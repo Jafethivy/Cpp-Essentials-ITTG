88665b9ab4884fdd2139884e9620d2b0d98c0774
Jafetivy <josejafethgonzalezlopez@gmail.com>
2025-08-31 00:40:42 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(){
+	int foot, choose;
+	float inch, meter;
+
+	cout << "choose 1 for Metric or 2 for imperial: ";
+	cin >> choose;
+
+	switch (choose){
+		case 1:	void Metrical_sys();
+		case 2: void Imperial_sys();
+	}
+
+
+
+	return 0;
+}
+
+void Metrical_sys() {
+	
+}
+
+void Imperial_sys() {
+}
